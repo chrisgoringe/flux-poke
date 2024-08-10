ccb2bdf3e4e22fd837d15fbc551e10166a2bfe92
Chris <chris.goringe@gmail.com>
2024-08-10 19:02:34 +1000

    args

--- push_to_hub.py
+++ push_to_hub.py
@@ -1,47 +1,41 @@
 import datasets
 import os
-from safetensors.torch import load_file
 from argparse import ArgumentParser
 from tqdm import tqdm
 
 from huggingface_hub import HfApi
 
+repo_id = lambda a : f"ChrisGoringe/{a}"
 
-def from_directory_of_files(directory, push_name):
+def create_repo(repo):
+    api = HfApi()
+    api.create_repo(repo_id=repo_id(repo), repo_type="dataset", private=True)
+
+def upload_files_from_directory(repo, directory):
     api = HfApi()
     for file in tqdm(os.listdir(directory)):
-        root, ext = os.path.splitext(file)
-        if ext=='.safetensors':  
+        if os.path.splitext(file)[1]=='.safetensors':  
             print(file)
             api.upload_file(
                 path_or_fileobj=os.path.join(directory, file),
                 path_in_repo=file,
-                repo_id=push_name,
+                repo_id=repo_id(repo),
                 repo_type="dataset",
             )
 
-    #for file in tqdm(os.listdir(directory)):
-    #    root, ext = os.path.splitext(file)
-    #    if ext=='.safetensors':  
-    #        datum = load_file(os.path.join(directory, file))
-    #        ds = datasets.Dataset.from_dict(datum)
-    #        ds.push_to_hub(push_name, split=f"p_{root}", num_shards=1)
-
-def from_dataset(directory, push_name):
+def upload_dataset(repo, directory):
     ds = datasets.Dataset.load_from_disk(directory)
-    ds.push_to_hub(push_name)
+    ds.push_to_hub(repo_id(repo),)
 
 if __name__=='__main__':
     a = ArgumentParser()
-    a.add_argument('--create', action='store_true')
-    a.add_argument('--hidden_states', action='store_true')
-    a.add_argument('--prompts', action='store_true')
+    b = a.add_mutually_exclusive_group(required=True)
+    b.add_argument('--create', action='store_true')
+    b.add_argument('--hidden_states', action='store_true')
+    b.add_argument('--prompts', action='store_true')
+    a.add_argument('--repo', required=True)
     args = a.parse_args()
 
-    if args.create:
-        api = HfApi()
-        api.create_repo(repo_id="ChrisGoringe/fi", repo_type="dataset", private=True)
-    if args.hidden_states:
-        from_directory_of_files(directory="hidden_states", push_name="ChrisGoringe/fi")
-    if args.prompts:
-        from_dataset(directory="prompts_dataset", push_name="ChrisGoringe/uncleaned_prompts")
+    if args.create:          create_repo(repo=args.repo)
+    elif args.hidden_states: upload_files_from_directory(repo=args.repo, directory="hidden_states")
+    elif args.prompts:       upload_dataset(repo=args.repo, directory="prompts_dataset")
