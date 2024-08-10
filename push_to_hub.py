7bffe583bdc9ddac41e6706b150f9e2e99b8cf4c
Chris <chris.goringe@gmail.com>
2024-08-10 17:48:26 +1000

    args

--- push_to_hub.py
+++ push_to_hub.py
@@ -1,6 +1,7 @@
 import datasets
 import os
 from safetensors.torch import load_file
+from argparse import ArgumentParser
 
 def from_directory_of_files(directory, push_name):
     def gen():
@@ -18,11 +19,13 @@ def from_dataset(directory, push_name):
     ds = datasets.Dataset.load_from_disk(directory)
     ds.push_to_hub(push_name)
 
-HF_NAME, DIRECTORY, ALREADY_DATASET = ("ChrisGoringe/flux_internals","hidden_states",False)
-#HF_NAME, DIRECTORY, ALREADY_DATASET = ("ChrisGoringe/uncleaned_prompts","prompts_dataset",True)
-
 if __name__=='__main__':
-    if ALREADY_DATASET:
-        from_dataset(directory=DIRECTORY, push_name=HF_NAME)
-    else:
-        from_directory_of_files(directory=DIRECTORY, push_name=HF_NAME)
+    a = ArgumentParser()
+    a.add_argument('--hidden_states', action='store_true')
+    a.add_argument('--prompts', action='store_true')
+    a.parse_args()
+
+    if a.hidden_states:
+        from_dataset(directory="hidden_states", push_name="ChrisGoringe/flux_internals")
+    if a.prompts:
+        from_directory_of_files(directory="prompts_dataset", push_name="ChrisGoringe/uncleaned_prompts")
