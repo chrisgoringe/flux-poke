8243e0b0708ab8fd7c9a2bb77021de673c9e7de6
Chris <chris.goringe@gmail.com>
2024-08-13 13:01:10 +1000

    minor

--- ztemp.py
+++ ztemp.py
@@ -8,10 +8,15 @@ fs = HfFileSystem()
 incomplete = True
 re = regex.compile("datasets/ChrisGoringe/fi/[0-9_]*.safetensors")
 
+to_do = len(fs.glob("/".join(("datasets",repo_id,"*"))))
 while incomplete:
+    done = 0
     try:
         for path in fs.glob("/".join(("datasets",repo_id,"*"))): 
             if re.match(path): fs.rm(path)
+            done += 1
+            print(f"\r{done}/{to_do}",end='')
         incomplete = False
     except HfHubHTTPError:
+        print("\nErrored")
         time.sleep(30)
